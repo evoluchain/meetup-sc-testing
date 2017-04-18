5fd27d6385dca108b0c0fb5336fa983489a5651b
Fabien Champigny <fabien@champigny.name>
2017-04-18 13:03:17 +0200

    Add test on Event disconnected from function call

--- test/HelloWorld.spec.js
+++ test/HelloWorld.spec.js
@@ -1,30 +1,43 @@
-var HelloWorld = artifacts.require("./HelloWorld.sol");
+var HelloWorld = artifacts.require("./HelloWorld.sol")
 
 contract('HelloWorld - demo', function (accounts) {
 
     it("should create an instance", function () {
         return HelloWorld.deployed()
             .then(function (instance) {
-                return instance.balanceOf.call(accounts[0]);
+                return instance.balanceOf.call(accounts[0])
             })
+            
             .then(function (balance) {
-                assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
-            });
-    });
+                assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account")
+            })
+    })
 
-    it('should create an Event', function () {
-        var instance;
-
+    it('should create an Event - simple for testing', function () {
         return HelloWorld.deployed()
-            .then(function (_instance) {
-                instance = _instance;
-                return   instance.transfer(accounts[1], 500)
+            .then(function (instance) {
+                return instance.transfer(accounts[1], 500)
             })
 
             .then(function (result) {
                 assert.equal(result.logs[0].event, "Transfer", "Expected Transfer event")
                 assert.equal(result.logs[0].args.value.valueOf(), 500)
-            });
-    });
+            })
+    })
 
-});
+    it('should create an Event - disconnected from instance call', function () {
+        return HelloWorld.deployed()
+            .then(function (instance) {
+                instance.transfer(accounts[1], 500)
+                return instance.Transfer()
+            })
+
+            .then(function (watcher) {
+                watcher.get(function(error, events){
+                    assert.equal(events[0].event, "Transfer", "Expected Transfer event")
+                    assert.equal(events[0].args.value.valueOf(), 500)
+                })
+            })
+    })
+
+})
