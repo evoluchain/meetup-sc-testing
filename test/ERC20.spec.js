c58d45509254e3a2c927fa0ed31dc4ac8c1759dc
Fabien Champigny <fabien@champigny.name>
2017-04-18 16:56:28 +0200

    Add Erc20 (#5)
    * Add nb of events check
    
    * Add ERC20 + spec draft

--- test/ERC20.spec.js
+++ test/ERC20.spec.js
@@ -1,20 +1,20 @@
-var HelloWorld = artifacts.require("./HelloWorld.sol")
+var TestToken = artifacts.require("./TestToken.sol")
 
-contract('HelloWorld - demo', function (accounts) {
+contract('ERC20', function (accounts) {
 
     it("should create an instance", function () {
-        return HelloWorld.deployed()
+        return TestToken.new(10000, 'Brouzouf', 1, 'BRX', {from: accounts[0]})
             .then(function (instance) {
                 return instance.balanceOf.call(accounts[0])
             })
-            
-            .then(function (balance) {
-                assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account")
+
+            .then(function (result) {
+                assert.strictEqual(result.toNumber(), 10000)
             })
     })
 
     it('should create an Event - simple for testing', function () {
-        return HelloWorld.deployed()
+        return TestToken.new(10000, 'Brouzouf', 1, 'BRX', {from: accounts[0]})
             .then(function (instance) {
                 return instance.transfer(accounts[1], 500)
             })
@@ -22,22 +22,22 @@ contract('HelloWorld - demo', function (accounts) {
             .then(function (result) {
                 assert.equal(result.logs.length, 1, "More than 1 event")
                 assert.equal(result.logs[0].event, "Transfer", "Expected Transfer event")
-                assert.equal(result.logs[0].args.value.valueOf(), 500)
+                assert.equal(result.logs[0].args._value.valueOf(), 500)
             })
     })
 
-    it('should create an Event - disconnected from instance call', function () {
-        return HelloWorld.deployed()
+    it.skip('should create an Event - disconnected from instance call', function () {
+        return TestToken.new(10000, 'Brouzouf', 1, 'BRX', {from: accounts[0]})
             .then(function (instance) {
                 instance.transfer(accounts[1], 500)
                 return instance.Transfer()
             })
 
             .then(function (watcher) {
-                watcher.get(function(error, events){
-                    assert.equal(events.length, 1, "More than 1 event")
+                watcher.get(function (error, events) {
+                    assert.equal(events.length, 1, "Not 1 event")
                     assert.equal(events[0].event, "Transfer", "Expected Transfer event")
-                    assert.equal(events[0].args.value.valueOf(), 500)
+                    assert.equal(events[0].args._value.valueOf(), 500)
                 })
             })
     })
