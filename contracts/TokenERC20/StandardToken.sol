ff2ed2446034221d3d5a704cedb445ad350e2de0
Fabien Champigny <fabien@champigny.name>
2017-04-20 17:01:37 +0200

    Break something

--- contracts/TokenERC20/StandardToken.sol
+++ contracts/TokenERC20/StandardToken.sol
@@ -5,12 +5,9 @@ import "./Token.sol";
 contract StandardToken is Token {
 
     function transfer(address _to, uint256 _value) returns (bool success) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
+        balances[msg.sender] -= _value;
+        //Transfer(msg.sender, _to, _value);
+        return true;
     }
 
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
